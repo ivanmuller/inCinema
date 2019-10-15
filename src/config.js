f040ac45bd9a5c7eb07f79c1004e3f2c437baa32
Iván Müller <ivanmuller2.0@gmail.com>
2019-10-15 20:04:16 -0300

    dev

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ export default {
   'dateIntervalrefresh': 2000,
   'rooms': ['14','25','11','Big one','kids'],
   'types': ['Imax 3D', 'Classic'],
+  'enableFirebase': false,
   'yearsMin': 1950,
   'api': {
     'moviedb': 'ed013855c309922c99e0175e84b6da07'
